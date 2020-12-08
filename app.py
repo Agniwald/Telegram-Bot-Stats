c57100a935b8ef1c62b797504ed19ec8a4ce1ee9
spaceshinobi <DSH01>
2020-12-08 13:55:12 +0200

    v1

---
+++ app.py
@@ -0,0 +1,81 @@
+from flask import Flask, render_template
+import datetime
+from datetime import datetime as dt
+import pymongo
+import random
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+app = Flask(__name__)
+# MongoDB
+cluster = pymongo.MongoClient(os.getenv('MONGO_DB_URL'))
+db = cluster["stats"]["stats"]
+
+
+def fetch(bot_name, delta):
+	# Get bot's data from last range
+	bot_data = db.find({"bot_name": bot_name})
+	bot_commands = db.distinct("command", {"bot_name": bot_name})
+	today = datetime.date.today()
+	if delta:
+		start_delta = datetime.timedelta(days=delta)
+		last_range = today - start_delta
+		bot_data_list = [d for d in bot_data if dt.strptime(d['date'], '%Y-%m-%d').date() >= last_range ]
+	else:
+		bot_data_list = [d for d in bot_data]
+
+	# Get sorted dates as labels
+	dates = set(d['date'] for d in bot_data_list)
+	dates_objects = [datetime.datetime.strptime(ts, "%Y-%m-%d") for ts in dates]
+	dates_objects.sort()
+	labels = [datetime.datetime.strftime(ts, "%Y-%m-%d") for ts in dates_objects]
+
+	# Pack needed data in datasets for Chart.js
+	datasets = []
+	for command in bot_commands:
+		r = random.randint(0, 255)
+		g = random.randint(0, 255)
+		b = random.randint(0, 255)
+
+		command_data = {
+			"label": command,
+			"backgroundColor": f'rgb({r}, {g}, {b})',
+			"borderColor": f'rgb({r}, {g}, {b})',
+			"data": []
+		}
+
+		for label in labels:
+			counter = 0
+			for d in bot_data_list:
+				if d['date'] == label and d['command'] == command:
+					counter += 1
+			command_data['data'].append(counter)
+
+		datasets.append(command_data)
+
+	return dict({"labels": labels, "datasets": datasets})
+
+
+@ app.route("/")
+def index():
+	bot_list = db.distinct("bot_name")
+	return render_template("index.html", bot_list=bot_list)
+
+
+@ app.route("/<string:bot_name>")
+def bot(bot_name):
+	bot_data = db.find({"bot_name": bot_name})
+	bot_data_list = [d for d in bot_data]
+	return str(bot_data_list)
+
+
+@ app.route("/<string:bot_name>/<int:delta>")
+def bot_week(bot_name, delta):
+	return fetch(bot_name, delta)
+
+
+if __name__ == '__main__':
+	app.run(debug=False)
---
+++ static/js/scripts.js
@@ -0,0 +1,62 @@
+var active = '';
+
+function show(labels_to_show, datasets_to_show){
+	var ctx = document.getElementById('myChart').getContext('2d');
+
+  var parsed_datasets = [];
+  for (var i = 0; i < datasets_to_show.length; i++) {
+    parsed_datasets.push({
+      label: datasets_to_show[i]['label'],
+      backgroundColor: datasets_to_show[i]['backgroundColor'],
+      borderColor: datasets_to_show[i]['borderColor'],
+      data: datasets_to_show[i]['data'],
+      fill: false
+    });
+  }
+
+  var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+    
+    // The data for our dataset
+    data: {
+        labels: labels_to_show,
+        datasets: parsed_datasets
+    },
+
+    // Configuration options go here
+    options: {}
+  });
+}
+
+function change_range(range){
+  var li = document.getElementById(range);
+  if (!li.classList.contains("active")){
+    var otherli = document.getElementsByTagName("li");
+
+    for (let i = 0; i < otherli.length; i++)
+      otherli[i].classList.remove("active")
+    
+    li.classList.add("active");
+  }
+}
+
+function fetch(bot, range){
+  if (bot == ''){
+    bot = active;
+  }
+
+  document.getElementById("name").innerHTML = "Fetching and processing data...";
+	var xhttp = new XMLHttpRequest();
+  xhttp.responseType = 'json';
+  xhttp.open('GET', "/" + bot + "/" + range, true);
+  xhttp.onload  = function() {
+    var jsonResponse = xhttp.response;
+    show(jsonResponse['labels'], jsonResponse['datasets']);
+    document.getElementById("name").innerHTML = bot;
+  };
+	xhttp.send();
+
+  change_range(range);
+  active = bot;
+}
